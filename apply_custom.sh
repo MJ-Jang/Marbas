29c91b85d32dd263ce1b863450d968fedd3617d4
MJ-Jang <xkxpa@korea.ac.kr>
2020-08-12 14:55:14 +0900

    update getting dic from blob in apply_custom.sh

--- apply_custom.sh
+++ apply_custom.sh
@@ -1,4 +1,6 @@
 # get custom dictionary from blob
+wget https://mlpipelinhub.blob.core.windows.net/models/POS_tagger/SystemDic/latest/CustomDic.csv
 
-python custom_systemdict.py --custom_path="CustomDict.csv"
+python custom_systemdict.py --custom_path="CustomDic.csv"
 mv mecab_custom_csv.pkl marbas/resources
+rm -rf CustomDic.csv
